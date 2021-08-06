6b65fc86a7289d1c54f1c47ff2dd089c631bfe1c
ceelogre <sam.nishimwe@yahoo.com>
2021-08-06 13:15:15 +0200

    feat: implement json upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const app = express()
 
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,6 +10,11 @@ app.get('/', (req, res) => {
   })
   }
 )
+app.post('/geodata', (req, res) => {
+  res.json({
+    message: req.body
+  })
+})
 const port = 9700
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
