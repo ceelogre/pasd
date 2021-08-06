f9c3cef4909004e2d034d308e1b7baadffdbd13b
ceelogre <sam.nishimwe@yahoo.com>
2021-08-06 11:03:07 +0200

    feat: implement file upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import express from 'express'
+import fileUpload from 'express-fileupload'
 const app = express()
 
-
+app.use(fileUpload(
+))
 app.get('/', (req, res) => {
   res.json({
     message: '6 foot'
   })
   }
 )
+
+app.post('/data', (req, res) => {
+  console.info(req.files.file.name)
+  res.json({
+    message: 'Received!'
+  })
+})
 const port = 9700
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
