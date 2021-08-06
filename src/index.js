2c8d6fecc1c2338db3bd36b9c59dab8acd84b916
ceelogre <sam.nishimwe@yahoo.com>
2021-08-07 00:36:05 +0200

    feat(WIP) parse data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
+import XLSX from 'xlsx'
+import path from 'path'
+const __dirname = path.resolve()
 const app = express()
 
-app.use(fileUpload(
-))
+app.use(fileUpload({
+  //Parse nesting
+}))
 app.get('/', (req, res) => {
   res.json({
     message: '6 foot'
@@ -12,11 +16,28 @@ app.get('/', (req, res) => {
 )
 
 app.post('/data', (req, res) => {
-  console.info(req.files.file.name)
-  res.json({
-    message: 'Received!'
+  let file = req.files.file
+  let uploadPath = __dirname +'/' + file.name
+  console.info(uploadPath, ' path')
+  // Read the file
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      console.error(err, ' error moving file')
+      return
+    }
+    console.info('File moved to ', uploadPath)
+    console.info((uploadPath))
+    const workbook = XLSX.readFile(file.name)
+    // get the first sheet name 
+    const sheetName = workbook.SheetNames[0]
+    const sheet = workbook.Sheets[sheetName]
+    console.info(sheet)
+    res.json({
+      message: '6 foot'
+    })
   })
 })
+
 const port = 9700
 app.listen(port, () => {
   console.log(`Server is running on ${port}`)
